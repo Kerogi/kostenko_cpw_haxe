fe210361fe80f884e6cd30feb92d5eb5d978f69f
Igor Kostenko <work.kerogi@gmail.com>
2012-02-06 14:07:03 +0200

    +calculation of vector X, and sum U @done

--- trunk/src/Main.hx
+++ trunk/src/Main.hx
@@ -1,7 +1,10 @@
 package ;
 
-
+#if neko
     import neko.Lib;
+#elseif cpp
+	import cpp.Lib;
+#end	
 
 /**
  * ...
@@ -10,9 +13,6 @@ package ;
 
 class Main 
 {
-	static var mattr_size:Int = 5;
-	static var mattr:Array<Float> = new Array<Float>();
-
 	static function fill_func(i:Int, j:Int):Float
 	{
 		return Math.pow(2, j - 1) * (Math.abs(j - 3) - 1.3) * Math.pow(2, j) * (i - 3.4) * (j / 3 - 1);
@@ -24,7 +24,7 @@ class Main
 		{
 			for (j in 0...size)
 			{
-				mattr.push(func(i+1, j+1));
+				mattr_array.push(func(i+1, j+1));
 			}
 		}
 	}
@@ -35,14 +35,14 @@ class Main
 		{
 			for (j in 0...size)
 			{
-				Lib.print(mattr[i * size + j]);
+				Lib.print(mattr_array[i * size + j]);
 				if (lastdiv || j < size-1) Lib.print(div);
 			}
 			Lib.print('\n');
 		}
 	}
 	
-	static function transpose_mattrix(mattr_array:Array<Float>, size:Int):Void
+	static function transform_mattrix(mattr_array:Array<Float>, size:Int):Void
 	{
 		for (i in 0...size)
 		{
@@ -50,28 +50,73 @@ class Main
 			{
 				for (k in j...(size-1))
 				{
-					var a:Float = mattr[i * size + k];
-					var b:Float = mattr[i * size + k + 1];
+					var a:Float = mattr_array[i * size + k];
+					var b:Float = mattr_array[i * size + k + 1];
 					if ( a < 0 && b >=0)
 					{
-						mattr[i * size + k] = b;
-						mattr[i * size + k + 1] = a;
+						mattr_array[i * size + k] = b;
+						mattr_array[i * size + k + 1] = a;
 					}
 				}
 			}
 		}
 	}
 	
+		
+	static function find_vector_X(mattr_array:Array<Float>, size:Int):Array<Float>
+	{
+		var vectorX:Array<Float> = new Array<Float>();
+		for (i in 0...size)
+		{
+			for (j in 0...size)
+			{
+				var res:Float = 0;
+				for (k in 0...size)
+				{
+					res = res + (mattr_array[k * size + j] * mattr_array[i * size + k]); 
+				}
+				vectorX.push(res);
+			}
+		}
+		return vectorX;
+	}
+	
+	static function calculate_sum(vector:Array<Float>):Float
+	{
+		var l:Int = vector.length;
+		var res:Float = 0;
+		for (i in 0...l)
+		{
+			res += vector[i] + vector[l - i -1];
+		}
+		return res;
+	}
+	
 	static function main() 
 	{
-		Lib.print("Filling matrix\n");
-		fill_mattrix(mattr, 5, fill_func );
-		Lib.print("\nPrinting matrix\n");
-		print_matrix(mattr, mattr_size, '\t');
-		Lib.print("\nTransforming matrix\n");
-		transpose_mattrix(mattr, mattr_size);
-		Lib.print("\nPrinting matrix\n");	
-		print_matrix(mattr, mattr_size, '\t');
+		var A_size:Int = 5;
+		var A:Array<Float> = new Array<Float>();
+		var X:Array<Float> = new Array<Float>();
+		var U:Float = 0;
+
+		Lib.println("Filling matrix");
+		fill_mattrix(A, A_size, fill_func );
+		Lib.println("Matrix 'A':");
+		print_matrix(A, A_size, '\t');
+		
+		Lib.println("\nTransforming matrix");
+		transform_mattrix(A, A_size);
+		Lib.println("Matrix 'A\'':");
+		print_matrix(A, A_size, '\t');
+		
+		Lib.println("\nCalculating X vector");
+		X = find_vector_X(A, A_size);
+		Lib.println("Vector 'X':"+X);
+		
+		
+		Lib.println("\nCalculating sum");
+		U = calculate_sum(X);
+		Lib.println("Sum 'U':"+U);		
 	}
 	
 }
